029ad888f0584d17374e77f1c324123edb153904
alexanderlarrosa <68875556+alexanderlarrosa@users.noreply.github.com>
2020-10-23 00:17:08 -0300

    Carito completo

--- js/cart.js
+++ js/cart.js
@@ -3,10 +3,58 @@
 //elementos HTML presentes.
 var subTotalCost=0;
 var totalProduct=0;
+var porcentajeComison=0.15;
 let total=0;
+let totalConEnvio=0;
 var currentCartArray=[];
 var currentCantArray=[];
+var verificarModal=false;
 
+function verificar(){
+  var validado=true;
+  if(verificarModal==false){
+    alert("Debe seleccionar un metodo de pago");
+    validado=false;
+  }else{
+    if(currentCartArray.length<1){
+      alert("No tiene productos en su carrito");
+      validado=false;
+    }
+  }
+  return validado;
+}
+
+function verificarPayPal(){
+  if(document.getElementById("input-paypal").value==""){
+    alert("Debe completar todos los datos");
+  }else{
+    document.getElementById("paypal").innerHTML=`<div class="text-center"><h5>PAGO PROCESADO CON EXITO</h5></div>`;
+    document.getElementById("title-pay-method").innerHTML=`<img src="https://i.imgur.com/yK7EDD1.png" width="80">`;
+    verificarModal=true;
+  }
+  
+}
+
+function verificarTarjeta(){
+    if(document.getElementById("input-card-name").value=="" || document.getElementById("input-card-number").value=="" || document.getElementById("input-card-date").value=="" || document.getElementById("input-card-cvv").value==""){
+      alert("Debe completar todos los datos");
+    }else{
+      document.getElementById("card-div").innerHTML=`<div class="text-center"><h5>PAGO PROCESADO CON EXITO</h5></div>`;
+      document.getElementById("title-pay-method").innerHTML=`<img src="https://i.imgur.com/sB4jftM.png" width="80">`;
+      verificarModal=true;
+    }
+}
+
+
+function deleteProduct(posicion){
+  currentCartArray.splice(posicion,1);
+  if(currentCantArray.length<1){
+    calcTotal(null,0);
+  }
+  showCartList();
+  updateCart();
+  
+}
 
 function updateCart(){
   subTotal=document.getElementsByClassName("subTotal");
@@ -15,7 +63,7 @@ function updateCart(){
   for(let i=0; i<currentCartArray.length; i++){
     let product=currentCartArray[i];
     product.count=cantidad[i].value;
-    let subTotal= product.unitCost*product.count;
+    let subTotal= calcSubtotal(product);
     document.getElementsByClassName("subTotal")[i].innerHTML=product.currency+" "+subTotal;
     calcTotal(product, subTotal);
     document.getElementById("total").innerHTML="UYU "+total;
@@ -26,13 +74,31 @@ function updateCart(){
 
 }
 
+function calcEnvio(){
+  let costoEnvio=(Math.round(total * porcentajeComison * 100) / 100);
+  totalConEnvio=total+costoEnvio;
+  document.getElementById("comissionText").innerHTML="$ "+costoEnvio;
+  document.getElementById("totalCostText").innerHTML="$ "+totalConEnvio;
+}
+
+function calcSubtotal(product){
+  return product.unitCost*product.count;
+  }
 
 function calcTotal(product,subTotal){
-  if(product.currency=="USD"){
-    total=total+subTotal*40;
+  if(product!=null){
+    if(product.currency=="USD"){
+      total=total+subTotal*40;
+    }else{
+      total=total+subTotal;
+    }
+    calcEnvio();
   }else{
-    total=total+subTotal;
+    total=0;
+    calcEnvio();
   }
+  document.getElementById("productCostText").innerHTML="$ "+total;
+
 }
 
 function showCartList(){
@@ -41,15 +107,16 @@ function showCartList(){
     let htmlContentToAppend2 = "";
     for(let i = 0; i < currentCartArray.length; i++){
         let product = currentCartArray[i];
-        let subTotal= product.unitCost*product.count;
+        let subTotal= calcSubtotal(product);
         calcTotal(product,subTotal);
         htmlContentToAppend2 += `
         <tr>
         <td><img src="${product.src}" width="100px"</td>
         <td>${product.name}</td>
         <td>${product.currency} ${product.unitCost}</td>
-        <td><input type="number" value="${product.count}" class="cartClass" min="0" max="1000" step="1"/></td>
+        <td><input type="number" value="${product.count}" class="cartClass" min="1" max="1000" step="1"/></td>
         <td><p class="subTotal" >${product.currency} ${subTotal}</p></td>
+        <td><button type="button" onclick="deleteProduct(${i})" class="btn btn-outline-danger btn-xs"><i class="fa fa-trash" aria-hidden="true"></i></button></td>
         </tr>`  
     }
     htmlContentToAppend += `
@@ -61,6 +128,7 @@ function showCartList(){
         <th scope="col">Costo</th>
         <th scope="col">Cantidad</th>
         <th scope="col">Subtotal</th>
+        <th scope="col">Eliminar producto</th>
       </tr>
     </thead>
     <tbody>
@@ -73,6 +141,7 @@ function showCartList(){
       <td></td>
       <td></td>
       <td><p id="total">UYU ${total}</p></td>
+      <td></td>
     </tr>
   </tfoot>
   </table>`       
@@ -90,6 +159,47 @@ document.addEventListener("input", function(event){
   */
 }, false);
 
+document.getElementById("premiumradio").addEventListener("change", function(){
+  porcentajeComison = 0.15;
+  calcEnvio();
+});
+
+document.getElementById("expressradio").addEventListener("change", function(){
+  porcentajeComison = 0.07;
+  calcEnvio();
+});
+
+
+
+document.getElementById("standardradio").addEventListener("change", function(){
+  porcentajeComison = 0.05;
+  calcEnvio();
+});
+
+/*
+document.getElementById("paypal-button").addEventListener("click", function() {
+  document.getElementById("contidionsModal").style.display="none";
+});
+*/
+
+/*document.getElementById("confirm-button").addEventListener("click", function(){
+  
+})
+
+*/
+document.getElementById("submit-button").addEventListener("submit",function(){
+  verificar();
+})
+
+document.getElementById("confirm-btn-card").addEventListener("click",function(){
+  verificarTarjeta();
+})
+
+document.getElementById("confirm-btn-paypal").addEventListener("click",function(){
+  verificarPayPal();
+})
+
+
 
 document.addEventListener("DOMContentLoaded", function(e){
   
@@ -98,5 +208,35 @@ document.addEventListener("DOMContentLoaded", function(e){
           currentCartArray=resultObj.data.articles;
             showCartList();
         }
-    });   
+    });  
 });
+
+
+
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.getElementsByClassName('needs-validation');
+  // Loop over them and prevent submission
+  var validation = Array.prototype.filter.call(forms, function(form) {
+  form.addEventListener('submit', function(event) {
+  if (form.checkValidity() === false) {
+    alert("Debe completar todos los datos");
+    event.preventDefault();
+    event.stopPropagation();
+  } else{
+    var verificado = verificar();
+    if(verificado==false){
+      event.preventDefault();
+      event.stopPropagation();
+    }else{
+      alert("Gracias por su compra");
+    }
+  }
+  form.classList.add('was-validated');
+  }, false);
+  });
+  }, false);
+  })();
